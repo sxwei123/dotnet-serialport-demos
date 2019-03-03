b4436eaf7767ef89c01feea4ad44a1443542c5b1
Leon <xw.song@outlook.com>
2019-03-03 17:01:52 +0800

    first commit

---
+++ serial-read/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace serial_read
+{
+    class Program
+    {
+        static SerialPort _serialPort;
+        static void Main(string[] args)
+        {
+            Console.Write("Port no: ");
+            string port = Console.ReadLine();
+            Console.Write("baudrate: ");
+            string baudrate = Console.ReadLine();
+            // Create a new SerialPort on port COM7
+            _serialPort = new SerialPort(port, int.Parse(baudrate));
+
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 1500;
+            _serialPort.WriteTimeout = 1500;
+
+            _serialPort.Open();
+
+
+            while (true)
+            {
+                Read();
+            }
+
+            _serialPort.Close();
+        }
+
+        public static void Read()
+        {
+            try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+            catch (TimeoutException) { }
+        }
+    }
+}
